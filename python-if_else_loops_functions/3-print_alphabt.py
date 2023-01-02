9274cedf641dd32751b93642a7074d3ac4ebfdb9
root <nikhil>
2023-01-01 23:08:09 -0800

    3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for in range(ord('a'), ord('z')+1):
-    a=chr(i)
+for i in range(ord('a'), ord('z')+1):
+    a = chr(i)
     if a not in "qe":
         print(a, end="" .format(chr))
