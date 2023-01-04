d1c4839ce79fafba100d43713a3926e07bf910c3
root <nikhil>
2023-01-04 02:06:53 -0800

    100-my_calculator.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, div, mul
     from sys import argv
-    if len(argv)! = 4:
+    if len(argv) != 4:
         print("./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     operator = argv[2]
@@ -10,11 +10,11 @@ if __name__ == "__main__":
     n2 = int(argv[3])
     if operator == "+":
         print("{} + {} = {}".format(n1, n2, add(n1, n2)))
-    elif operator =="-":
-        print("{} - {} = {}".format(n1, n2, sub(n1, n2)))
-    elif operator =="*":
+    elif operator == "-":
+        print("{:d} - {:d} = {:d}".format(n1, n2, sub(n1, n2)))
+    elif operator == "*":
         print("{} * {} = {}".format(n1, n2, mul(n1, n2)))
-    elif operator =="/":
+    elif operator == "/":
         print("{} / {} = {}".format(n1, n2, div(n1, n2)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
