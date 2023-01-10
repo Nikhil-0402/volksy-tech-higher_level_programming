1384ebafd140cd78fff6eb6748046b4b03122dfe
root <nikhil>
2023-01-09 21:29:16 -0800

    100-safe_print_integer_err.py

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
     import sys
-       try:
+        try:
            print("{:d}".format(value))
            return True
        except Exception as e:
