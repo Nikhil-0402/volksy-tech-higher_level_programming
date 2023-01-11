6455dfde5952ba28cdf5d7b0e55fcd2605b79499
root <nikhil>
2023-01-11 02:06:58 -0800

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module creates a class named Square"""
+
+
+class Square:
+    """A class named Square"""
+    Attributes:
+    attrl (size): size of Square
+    """
+    def__init__(self, size):
+       """
+       Args:
+       size: size for __size attribute of class instance
+       """
+       self.__size = size
