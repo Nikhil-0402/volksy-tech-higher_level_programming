a8c61ab89df4e59aa580e5cf865c16a041b97196
root <nikhil>
2023-01-11 02:21:17 -0800

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,14 +3,6 @@
 
 
 class Square:
-    """A class named Square"""
-
-    Attributes:
-    attrl (size): size of Square
-    """
+    """A class"""
     def__init__(self, size):
-       """
-       Args:
-       size: size for __size attribute of class instance
-       """
        self.__size = size
