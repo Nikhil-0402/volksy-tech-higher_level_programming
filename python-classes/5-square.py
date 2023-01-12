95d0e9b6c6046fc0af0ed1734fcff2d4edeb62e2
root <nikhil>
2023-01-11 20:50:23 -0800

    5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -6,9 +6,9 @@ class Square:
     """ square """
     def __init__(self, size=0):
         self.__size = size
-    
+
     def area(self):
-        return self.size__ * self.__size
+        return self.__size * self.__size
 
     @property
     def size(self):
