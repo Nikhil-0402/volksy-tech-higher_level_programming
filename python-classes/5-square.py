916e56a5633a407bec2bd66799fdff502cba95e9
root <nikhil>
2023-01-11 20:19:37 -0800

    5-square.py

---
+++ python-classes/5-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This module"""
+
+
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+    
+    def area(self):
+        return self.size__ = self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >=0")
+        self.__size = value
+
+    def my_print(self):
+        if self.__size > 0:
+            for row in range(self.__size):
+                for column in range(self.__size):
+                    print("#", end="")
+                print()
+        else:
+            print()
