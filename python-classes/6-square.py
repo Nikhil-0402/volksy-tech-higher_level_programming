f84e7d7c74112c95a1435b50e0524003ad378c41
root <nikhil>
2023-01-11 21:30:51 -0800

    6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-""" 6-Square """
+"""This module creates a class named Square"""
 
 
 class Square:
-    """ A code """
-
+    """A class """
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
-    
+
     def area(self):
         return self.__size ** 2
 
@@ -24,16 +23,16 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-   def my_print(self):
-       if self.__size > 0:
-           for row in range(self.position[1]):
-               print()
-           for row in range(self.__size):
-               for column in range(self.__position[0]):
-                   print(" ", end="")
-               for column in range(self.__size):
-                   print("#", end="")
-               print()
+    def my_print(self):
+        if self.__size > 0:
+            for row in range(self.__position[1]):
+                print()
+            for row in range(self.__size):
+                for column in range(self.__position[0]):
+                    print(" ", end="")
+                for column in range(self.__size):
+                    print("#", end="")
+                print()
         else:
             print()
 
@@ -50,4 +49,3 @@ class Square:
         if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
-
