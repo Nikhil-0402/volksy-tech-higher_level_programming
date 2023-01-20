55c87cf44c0c2bdd9be3dffafe5d765962c8ece9
root <nikhil>
2023-01-19 20:29:50 -0800

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,12 +1,54 @@
+
 #!/usr/bin/python3
-"""save_to_json_file
-"""
+
+"""Module for saving to json"""
+
+
+
+
+
+
+
 import json
 
+import os.path
 
-def save_to_json_file(my_obj, filename):
-    """Writes an object to a text file, using JSON
-    """
+import sys
 
-    with open(filename, mode="w", encoding="UTF-8") as saveFile:
-        json.dump(my_obj, saveFile)
+from sys import argv
+
+
+
+
+
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+
+
+filename = "add_item.json"
+
+json_list = []
+
+
+
+
+if os.path.exists(filename):
+
+    json_list = load_from_json_file(filename)
+
+
+
+
+for index in argv[1:]:
+
+    json_list.append(index)
+
+
+
+
+save_to_json_file(json_list, filename)
