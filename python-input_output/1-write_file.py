dfffcb03a7fae7e1c545c598141cfb8ab1d4f8b6
root <nikhil>
2023-01-20 00:58:00 -0800

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-# 1-number_of_lines.py
-# Brennan D Baraban <375@holbertonschool.com>
-"""Defines a text file line-counting function."""
+""" write file"""
 
 
-def number_of_lines(filename=""):
-    """Return the number of lines in a text file."""
-    lines = 0
-    with open(filename) as f:
-        for line in f:
-            lines += 1
-    return lines
+def write_file(filename="", text=""):
+    """ lines count """
+    with open(filename, 'w', encoding='utf-8')as file:
+        return file.write(text)
