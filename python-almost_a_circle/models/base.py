3480ef11492ab168dd2ce46cc65502e19bf7f789
root <nikhil>
2023-01-23 19:32:31 -0800

    base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -166,70 +166,35 @@ class Base:
             return object
 
 
-
-
         if cls.__name__ == "Square":
-
             object = cls(1)
-
             object.update(**dictionary)
-
             return object
 
-
-
-
     @classmethod
-
     def load_from_file(cls):
-
         """loads a list of instances from a json file"""
-
         if path.exists(cls.__name__ + ".json") is False:
-
             return []
-
         with open(cls.__name__ + ".json", "r",  encoding='utf-8') as file:
-
             listofinstances = []
-
             objectlist = cls.from_json_string(file.read())
-
             for dict in objectlist:
-
                 objectdict = {}
-
                 for key, value in dict.items():
-
                     objectdict[key] = value
-
                 listofinstances.append(cls.create(**objectdict))
-
             return listofinstances
 
-
-
-
     @classmethod
-
     def load_from_file_csv(cls):
-
         if path.exists(cls.__name__ + ".csv") is False:
-
             return []
-
         with open(cls.__name__ + ".csv", "r", newline='') as f:
-
             listofinstances = []
-
             reader = csv.DictReader(f)
-
             for row in reader:
-
                 for key, value in row.items():
-
                     row[key] = int(value)
-
                 listofinstances.append(cls.create(**row))
-
         return listofinstances
