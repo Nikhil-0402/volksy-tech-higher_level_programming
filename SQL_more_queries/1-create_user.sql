de0f2367e696f3dc18e1f8561699e332bc3920bc
root <nikhil>
2023-02-01 21:19:43 -0800

    1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
--- craete database
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+-- create mysql server
+CREATE USER IF NOT EXISTS'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
