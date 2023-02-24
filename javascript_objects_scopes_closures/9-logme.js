c4fdd25fcfdbb7e81bbb9a8912b119d2def0bec4
kodenik <nikhilb@koderoom.com>
2023-02-24 03:18:52 -0800

    9-logme.js

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-// function that returns the reversed version of a list withouth reverse
+// prints the number of arguments already printed and the new argument value.
 
-exports.esrever = function (list) {
-  const reversedList = [];
-  const last = list.length - 1;
-  for (let i = last; i >= 0; i--) {
-    reversedList.push(list[i]);
-  }
-  return (reversedList);
+let i = 0;
+exports.logMe = function (item) {
+  console.log(i + ': ' + item);
+  i++;
 };
