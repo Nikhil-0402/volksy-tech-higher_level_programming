796982ec6cf3782e8f6ba70386315d0901e653a8
kodenik <nikhilb@koderoom.com>
2023-02-24 03:38:27 -0800

    100-map.js

--- javascript_objects_scopes_closures/100-map.js
+++ javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // script that imports an array and computes a new array.
 
-const orginallist = require('100-data.js').list;
-console.log(orginalList);
-const mappedList = orginalList.map (function (e, index) {
+const originalList = require('100-data.js').list;
+console.log(originalList);
+const mappedList = originalList.map (function (e, index) {
     return (e * index);
 });
 console.log(mappedList);
